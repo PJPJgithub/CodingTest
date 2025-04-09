411dca471e3b2186a046d500c9033f1fbafd533c
JungyuPark <gue0312@gmail.com>
2025-04-10 00:15:25 +0900

    2025.04.09 home

--- solved_class2/숫자_카드_2.py
+++ solved_class2/숫자_카드_2.py
@@ -1,12 +1,26 @@
 #https://www.acmicpc.net/problem/10816
 
+'''
+Counter 사용법
+
+from collections import Counter : 딕셔너리 형태로 리스트의 각 원소가 몇개 있는지 알려준다.
+
+count = Counter(?)
+count[1] : key값이 1인 value
+'''
+
 import sys
+from collections import Counter
 
 sys.stdin=open("input.txt",'r')
 
 n = int(input())
-sang = list(map(int,input()))
+sang = list(map(int,input().split()))
 
 m = int(input())
-arr = list(map(int,input()))
+arr = list(map(int,input().split()))
 
+sang_cnt = Counter(sang)
+
+for i in arr:
+  print(sang_cnt[i], end=' ')
